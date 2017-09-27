7c9cfd3de8c77805a1f19532dba4aa64b9dbc8d7
9thflr-Welly <32316376+9thflr-Welly@users.noreply.github.com>
2017-09-27 11:41:58 +0800

    update profile.js
    divide the profSubmitProfile() function into 3 parts corresponding to 3 Bootstrap modals

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -29,7 +29,9 @@ $(document).ready(function() {
   setTimeout(loadProf, 1000);
 
   $(document).on('click', '#prof-edit', profEdit); //打開modal
-  $(document).on('click', '#prof-submit', profSubmit); //完成編輯
+  $(document).on('click', '#prof-submit-action', profSubmitAction); //完成編輯-action
+  $(document).on('click', '#prof-submit-profile', profSubmitProfile); //完成編輯-profile
+  $(document).on('click', '#prof-submit-basic', profSubmitBasic); //完成編輯-basic
   $('#profModal').on('hidden.bs.modal', profClear); //viewModal 收起來
   $(document).on('click', '#signout-btn', logout); //登出
 //----------------TAG---------------
@@ -265,23 +267,55 @@ function profEdit() {
 }
 
 function checkInputUndefined(input){
-	if(typeof(input) === "undefined"){    	
+    if(typeof(input) === "undefined"){    	
     	return 0
     }
     else{
     	return input;
     }
 }
-  
-function profSubmit() {
+
+function profSubmitBasic() {
   let userId = auth.currentUser.uid;
-  let id = checkInputUndefined($('#prof-edit-id').val());
-  let name = checkInputUndefined($('#prof-edit-name').val());
-  let nick = checkInputUndefined($('#prof-edit-nick').val());
+  // console.log(id, name, dob, email, gender,phone);
+  let company = checkInputUndefined($('#prof-edit-company').val());
+  let logo = checkInputUndefined($('#prof-edit-logo').val());
+  // console.log(id);
+  // database.ref('users/' + userId).remove();
+  database.ref('users/' + userId).update({
+    company: company,
+    logo: logo
+  });
+
+  $('#error-message').hide();
+  profClear();
+  loadProf();
+  $('#profModal').modal('hide');
+}
+	
+function profSubmitAction() {
+  let userId = auth.currentUser.uid;
   let dob = checkInputUndefined($('#prof-edit-dob').val());
   let email = checkInputUndefined($('#prof-edit-email').val());
   let gender = checkInputUndefined($('#prof-edit-gender').val());
   let phone = checkInputUndefined($('#prof-edit-phone').val());
+  // console.log(id);
+  // database.ref('users/' + userId).remove();
+  database.ref('users/' + userId).update({
+    dob: dob,
+    email: email,
+    gender: gender,
+    phone: phone,
+  });
+
+  $('#error-message').hide();
+  profClear();
+  loadProf();
+  $('#profModal').modal('hide');
+}
+	
+function profSubmitProfile() {
+  let userId = auth.currentUser.uid;
 
   let chanId_1 = checkInputUndefined($('#prof-edit-channelId_1').val());
   let chanSecret_1 = checkInputUndefined($('#prof-edit-channelSecret_1').val());
@@ -290,25 +324,15 @@ function profSubmit() {
   let chanSecret_2 = checkInputUndefined($('#prof-edit-channelSecret_2').val());
   let chanAT_2 = checkInputUndefined($('#prof-edit-channelAccessToken_2').val());
   // console.log(id, name, dob, email, gender,phone);
-  let company = checkInputUndefined($('#prof-edit-company').val());
-  let logo = checkInputUndefined($('#prof-edit-logo').val());
   // console.log(id);
   // database.ref('users/' + userId).remove();
-  database.ref('users/' + userId).set({
-    name: name,
-    dob: dob,
-    email: email,
-    gender: gender,
-    phone: phone,
-    nickname: nick,
+  database.ref('users/' + userId).update({
     chanId_1: chanId_1,
     chanSecret_1: chanSecret_1,
     chanAT_1: chanAT_1,
     chanId_2: chanId_2,
     chanSecret_2: chanSecret_2,
     chanAT_2: chanAT_2,
-    company: company,
-    logo: logo
   });
   io.connect().emit('update bot', [
     {
